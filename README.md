cb69ed4b8db645d72193ac46041ad4b7b38c4fcb
Jim Barton <jimhbarton@gmail.com>
2024-08-20 14:30:32 -0500

    GGv1.17 updates

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Example gRPC services that implement the interface used by Envoy's [External Processing filter](https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/ext_proc_filter)
 
-Currently the examples in this repo are only used for [Gloo Edge](https://github.com/solo-io/solo-projects) end-to-end tests.
+Currently the examples in this repo are used to support Gloo Gateway [ExtProc documentation](https://docs.solo.io/gateway/latest/traffic-management/extproc/header-manipulation/) and Solo [blogs](https://www.solo.io/blog/extproc-info-leakage/).
+
+These examples should only be considered sample code for Gloo Gateway enterprise users. They are not a supported component of any Gloo Gateway product. Do NOT use any of these examples in production systems.
--- basic-sink/basic-demo.sh
+++ basic-sink/basic-demo.sh
@@ -3,17 +3,23 @@
 set -e
 
 CLUSTER=test
-GLOO_EE_VERSION=v1.15.2
+GLOO_EE_VERSION=v1.17.0
 
 # Create a kind cluster:
 kind create cluster --name $CLUSTER
 
-# Install a recent version of Gloo Edge, with a values file that initializes extproc settings:
-helm install -n gloo-system gloo-ee gloo-ee/gloo-ee --create-namespace --set-string license_key=$GLOO_LICENSE_KEY --set gloo-fed.enabled=false --version $GLOO_EE_VERSION -f resources/values.yaml
+# Install Gateway API CRDs
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+
+# Install a recent version of Gloo Gateway, with a values file that initializes extproc settings:
+helm install -n gloo-system gloo-gateway glooe/gloo-ee --create-namespace --set-string license_key=$GLOO_LICENSE_KEY --set gloo-fed.enabled=false --version $GLOO_EE_VERSION -f resources/values.yaml
 
 # Apply the Deployment and Service for the extproc service (note that if you are using a locally-built image, you may need to change the image tag in the below yaml before applying it):
 kubectl apply -f resources/extproc.yaml
 
-# Create a VirtualService that routes to httpbin:
+# Create an HTTP gateway
+kubectl apply -f resources/gateway.yaml
+
+# Create an HTTPRoute that routes to httpbin:
 kubectl apply -f resources/httpbin.yaml
-kubectl apply -f resources/vs.yaml
+kubectl apply -f resources/httproute.yaml
